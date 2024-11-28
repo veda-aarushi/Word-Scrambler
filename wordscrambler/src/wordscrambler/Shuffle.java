8d290f3abdc3b675d373b7d4f2bc96301d896a84
veda-aarushi <121644311+veda-aarushi@users.noreply.github.com>
2024-11-27 16:43:14 -0800

    Shuffler.java

--- wordscrambler/src/wordscrambler/Shuffle.java
+++ wordscrambler/src/wordscrambler/Shuffle.java
@@ -1,56 +1,42 @@
 package wordscrambler;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
 public class Shuffle {
-	 public static void main(String[] args) {
-			    Scanner myObj = new Scanner(System.in);  
-			    System.out.println("Enter an input word:");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);  
+        System.out.println("Enter an input word:");
+        String input = scanner.nextLine();  
 
-			    String input  = myObj.nextLine();  
-	 String output = shuffle(input);
-	 System.out.println(output);
-	 }
+        String shuffledWord = shuffle(input);
+        System.out.println("Shuffled word: " + shuffledWord);
+    }
 
-	 public static String shuffle(String input){
-	        int i = 0;
-	        ArrayList<Character> chars = new ArrayList<Character>(input.length());
-	        String output = "";
-	        char[] characters = input.toCharArray();
-	        char[] newWord = new char[input.length() + 1];
-	        
-	        
-	        
-	        newWord[0] = characters[0];
-	        newWord[input.length()-1] = characters[input.length()-1];
-	        
-	        for (i=1; i < input.length()-1;i++ ) {
-	            chars.add(characters[i]);
-	        }
-	        
-	        System.out.println(chars);
-	        for (i = 1; i <= input.length()-2; i++)
-	        {
-	            Collections.shuffle(chars);
-	        }
-	        Character[] middle = chars.toArray(new Character[chars.size()]);
-	        for (i = 0; i < newWord.length - 2; i++)
-	        {
-	            newWord[i] = middle[i];
-	        }
-	        
-	    System.out.println(newWord);
-	        StringBuffer r= new StringBuffer(output);
-	        for (i = 0; i < newWord.length - 1; i++)
-	        {
-	        r.append(newWord[i]);
-	        }
-	        
-	        return output;
-	    }
+    public static String shuffle(String input) {
+        // Handle short inputs that cannot be shuffled meaningfully
+        if (input.length() <= 2) {
+            return input;
+        }
+
+        // Create a list for the middle characters
+        ArrayList<Character> middleChars = new ArrayList<>();
+        for (int i = 1; i < input.length() - 1; i++) {
+            middleChars.add(input.charAt(i));
+        }
+
+        // Shuffle the middle characters
+        Collections.shuffle(middleChars);
+
+        // Build the shuffled word
+        StringBuilder shuffledWord = new StringBuilder();
+        shuffledWord.append(input.charAt(0)); // Append the first character
+        for (char middleChar : middleChars) {
+            shuffledWord.append(middleChar);
+        }
+        shuffledWord.append(input.charAt(input.length() - 1)); // Append the last character
+
+        return shuffledWord.toString();
+    }
 }
-
-
